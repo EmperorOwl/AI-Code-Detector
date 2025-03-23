2b4db60a030488660605e90ba78b3cb3b682262c
EmperorOwl <ray@raylin.dev>
2025-03-23 17:46:50 +1100

    fix warm up steps

--- model.py
+++ model.py
@@ -33,16 +33,15 @@ class Model:
         test_dataset = CodeDataset(self.tokenizer, Model.TEST_DATASET)
         train_args = TrainingArguments(
             output_dir='./results',
-            num_train_epochs=2,
+            num_train_epochs=3,
             per_device_train_batch_size=16,
             per_device_eval_batch_size=16,
-            warmup_steps=500,
+            warmup_steps=15,
             weight_decay=0.01,
             logging_dir='./logs',
-            logging_steps=10,
+            logging_steps=25,
             optim='adamw_torch',
             learning_rate=5e-5,
-            save_total_limit=2,
         )
         trainer = Trainer(
             model=self.model,
