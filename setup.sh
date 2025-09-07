ba7bfaed13f831fe4c30515a1ee4ad3c4cbb060d
EmperorOwl <ray@raylin.dev>
2025-09-07 11:48:22 +0000

    setup script

---
+++ setup.sh
@@ -0,0 +1,11 @@
+pip install pandas
+pip install transformers
+pip install torch[transformers]
+pip install scikit-learn
+pip install tree-sitter
+pip install tree-sitter-java
+pip install tree-sitter-python
+
+mkdir outputs
+mkdir outputs/codebert_model
+mkdir outputs/ast_model
